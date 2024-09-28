16bd8205763605e797c181cc6a36414be79e99dd
Olaniyi Salami <salaminiyi@yahoo.com>
2024-09-28 12:57:21 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+import pathlib
+
+
+def main():
+    print(0)
+
+if __name__ == "__main__":
+    main()
